bc25de90fd174636c19a5046e27e1685389ba2a2
Ben-Hur <lukbenhur@gmail.com>
2022-04-15 18:53:31 -0300

    Prova APS Commit Inicial

---
+++ ExerciciosAps/src/listaAPS/Exercicio5.java
@@ -0,0 +1,27 @@
+package listaAPS;
+
+import java.util.Scanner;
+
+/*Faça um programa que peça o tamanho de um arquivo para download (em MB) e a 
+velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado 
+de download do arquivo usando este link (em minutos).
+*/
+public class Exercicio5 {
+	public static void main(String[] args) {
+		double arq , velocidade,download,downloadTempo;
+		Scanner dado = new Scanner(System.in);
+		
+		System.out.println("Qual o tamanho do arquivo?");
+		arq = dado.nextDouble();
+		System.out.println("Qual a velocidade da sua rede?");
+		velocidade = dado.nextDouble();
+		dado.close();
+		download = arq / (velocidade / 8); //taxa de transferencia em MB/s
+		downloadTempo = download / 60; //transformar em minutos
+		
+		System.out.printf("O arquivo de tamanho %.2f MB em uma rede de "
+				+ "velocidade de %.1f Mbps irá demorar cerca de %.2f Min. para ser baixado",arq,velocidade,downloadTempo);
+	
+		
+	}
+}
